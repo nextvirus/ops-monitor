8b96e8032b71d6839e972a54187433e6156bb7cc
zhangshihao <you@example.com>
2024-05-07 16:05:48 +0800

    first commit

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
             }
 
             // 计算总内存使用率，包括物理内存和交换内存
-            totalMemUsage := uint64(rss) + uint64(vmSwap) + uint64(VmSize)
+            totalMemUsage := uint64(rss) + uint64(vmSwap) 
             memUsagePercent := float64(totalMemUsage) / float64(totalMemory) * 100
 
             fmt.Printf("\rMemory Usage of PID %d: %s (%.2f%%)", pid, mem.HumanizeBytes(uint64(totalMemUsage)*1024), memUsagePercent)
